28a5fce4ef74b1b48aa1f49d6d318bb5d66db1a1
DAVID BOAZ <DAVIDBO@il.ibm.com>
2025-05-04 11:57:34 +0300

    simplify

--- tests/gen_dmn_rules.py
+++ tests/gen_dmn_rules.py
@@ -8,19 +8,15 @@ from loguru import logger
 from op_only_oas import op_only_oas
 from policy_adherence.common.open_api import read_openapi
 from policy_adherence.dmn.gen_rules import generate_tools_check_rules
-from run_gen import load_policy_new
+from run_gen import load_tool_policy
 
 
 async def gen_all():
     oas_path = "/Users/davidboaz/Documents/GitHub/tau_airline/input/openapi.yaml"
-    tool_names = [
-        # "cancel_reservation",
-        "book_reservation"
-    ]
-    policy_paths = [
-        # "/Users/davidboaz/Documents/GitHub/tau_airline/input/CancelReservation.json"
-        "/Users/davidboaz/Documents/GitHub/tau_airline/input/BookReservation.json"
-    ]
+    tool_policy_paths = {
+        "cancel_reservation": "/Users/davidboaz/Documents/GitHub/tau_airline/input/CancelReservation.json",
+        "book_reservation": "/Users/davidboaz/Documents/GitHub/tau_airline/input/BookReservation.json"
+    }
     output_dir = "/Users/davidboaz/Documents/GitHub/tau_airline/output"
     now = datetime.now()
     out_folder = os.path.join(output_dir, now.strftime("%Y-%m-%d_%H_%M_%S"))
@@ -29,9 +25,9 @@ async def gen_all():
     oas = read_openapi(oas_path)
     op_oas = op_only_oas(oas, "book_reservation")
 
-    tool_policies = [load_policy_new(path, tool_name) 
-        for tool_name, path 
-        in zip(tool_names, policy_paths)]
+    tool_policies = [load_tool_policy(tool_policy_path, tool_name) 
+        for tool_name, tool_policy_path 
+        in tool_policy_paths.items()]
     
     # domain = OpenAPICodeGenerator(cwd)\
     #     .generate_domain(oas_path, "domain.py")
--- tests/run_gen.py
+++ tests/run_gen.py
@@ -24,7 +24,7 @@ def read_oas(file_path:str)->OpenAPI:
         d = yaml.safe_load(file)
     return OpenAPI.model_validate(d)
 
-def load_policy_new(file_path:str, tool_name:str)->ToolPolicy:
+def load_tool_policy(file_path:str, tool_name:str)->ToolPolicy:
     with open(file_path, "r") as file:
         d = json.load(file)
     
@@ -48,25 +48,19 @@ def symlink_force(target, link_name):
 
 async def gen_all():
     oas_path = "/Users/davidboaz/Documents/GitHub/tau_airline/input/openapi.yaml"
-    tool_names = [
-        "cancel_reservation",
-        # "book_reservation"
-    ]
-    policy_paths = [
-        "/Users/davidboaz/Documents/GitHub/tau_airline/input/CancelReservation.json"
-        # "/Users/davidboaz/Documents/GitHub/tau_airline/input/BookReservation.json"
-    ]
+    tool_policy_paths = {
+        "cancel_reservation": "/Users/davidboaz/Documents/GitHub/tau_airline/input/CancelReservation.json",
+        "book_reservation": "/Users/davidboaz/Documents/GitHub/tau_airline/input/BookReservation.json"
+    }
     output_dir = "/Users/davidboaz/Documents/GitHub/tau_airline/output"
     now = datetime.now()
     out_folder = os.path.join(output_dir, now.strftime("%Y-%m-%d_%H_%M_%S"))
     os.makedirs(out_folder, exist_ok=True)
 
-    tool_policies = [load_policy_new(path, tool_name) 
-        for tool_name, path 
-        in zip(tool_names, policy_paths)]
+    tool_policies = [load_tool_policy(tool_policy_path, tool_name) 
+        for tool_name, tool_policy_path 
+        in tool_policy_paths.items()]
     
-    # domain = OpenAPICodeGenerator(cwd)\
-    #     .generate_domain(oas_path, "domain.py")
     result = await generate_tools_check_fns("my_app", tool_policies, out_folder, oas_path)
 
     print(f"Domain: {result.domain_file}")
@@ -75,34 +69,9 @@ async def gen_all():
         for test in tool.test_files:
             print(f"\t{test.file_name}")
     
-
-async def gen_tool_check_fn(case:str):
-    cwd = f"tau_airline/output/{case}"
-    tool_name = "book_reservation"
-    policy_path = "tau_airline/input/BookReservation.json"
-    tool = load_policy_new(policy_path, tool_name)
-    item = tool.policy_items[0]
-    # domain = SourceFile.load_from(os.path.join(cwd, "domain.py"))
-    # check_fn = SourceFile.load_from(os.path.join(cwd, "check_book_reservation.py"))
-    # tests = [
-    #     SourceFile.load_from(os.path.join(cwd, "test_check_Baggage Allowance.py")),
-    #     SourceFile.load_from(os.path.join(cwd, "test_check_Passenger Information.py")),
-    #     SourceFile.load_from(os.path.join(cwd, "test_check_Payment Method Restrictions.py")),
-    # ]
-    gen = ToolCheckPolicyGenerator(tool, cwd)
-    items = [item]
-    check_futures = [
-        gen.improve_tool_item_check_fn_loop(tool_item, check_fn, test)
-        for tool_item in items 
-    ]
-    checks = await asyncio.gather(*check_futures)
-    # res = await gen._generate_tool_check_fn(domain, check_fn, tool, test)
-    print(checks)
-
 if __name__ == '__main__':
     load_dotenv()
     logger.remove()
     logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> <level>{message}</level>")
 
     asyncio.run(gen_all())
-    # asyncio.run(gen_tool_check_fn("game"))
